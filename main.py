62974516cd68b8cd9afdaf09317fdb11c272f0d9
OsvaldoVillar185 <osvaldovillar18.5@gmail.com>
2026-03-09 14:24:44 -0600

    Segundo commit

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ def clean_value(value):
         if char in valid_chars:
             result +=char
     return result
+
+def convert_int (texto):
+    ##Si esta vacio entonces devuelve 0
+    if texto == "":
+        return 0
+    ##Se intenta convertir el texto a un numero y si no se puede entonces se devuelve 0
+    try:
+        number=float(texto)
+        return int(number)
+    except ValueError:
+        return 0
